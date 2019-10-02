2a551698e9ec0843d0fd62d9a9cceac47eeedec8
Josué Francisco <josuefrancisco2017@outlook.pt>
2019-10-02 01:23:09 +0100

    create models controllers and conection to mongodb

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.get('/', (req, res) =>{
-  res.end("Hello Word")
-})
+mongoose.connect(
+  'mongodb+srv://francisco15:francisco15@aircnc-qa5xh.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(process.env.PORT || 3000)
