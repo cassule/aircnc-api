aa86575096c50da71bd9df66eef0e2156c33c250
Josué Francisco <josuefrancisco2017@outlook.pt>
2019-10-04 06:05:20 +0100

    add cors for allow access api

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 
@@ -12,7 +13,8 @@ mongoose.connect(
   }
 )
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3333)
